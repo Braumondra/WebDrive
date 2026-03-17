a30dbed65f18249e11be00fdc46fbd587a8c929e
braumo <ondrabraum@seznan.cz>
2026-03-17 08:45:36 +0100

    dsa

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -12,8 +12,8 @@ Given(/^I am on the (\w+) page$/, async (page) => {
     await pages[page].open()
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await LoginPage.login(username, password)
+When(/^I login with (\w+) and (.+)$/, async (karel, král) => {
+    await LoginPage.login(karel, král)
 });
 
 Then(/^I should see a flash message saying (.*)$/, async (message) => {
